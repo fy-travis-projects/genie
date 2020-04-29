93578f2828248cc492f206a125a7b2095fbf2a84
zfy <rachmanino@yahoo.com>
2020-04-29 18:50:04 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,12 +11,12 @@ chmod o-wx ~/.ssh/config
 # sudo apt-get install -y pigz
 # cd ~/
 # tar -cf repo.tar.gz -I pigz .gradle
-cd ~/build/penelope24
+cd ~/build/penelope24/.gradle
 ls -al
 
-dirs=(~/build/penelope24/*/)
-name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
-echo $dirs
-echo $name
+# dirs=(~/build/penelope24/*/)
+# name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+# echo $dirs
+# echo $name
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" ~/repo.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
