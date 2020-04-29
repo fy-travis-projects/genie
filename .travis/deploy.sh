6a2defb0f0cbfebabe9a2250230b48ff2f367cf8
zfy <rachmanino@yahoo.com>
2020-04-30 00:37:50 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,13 +7,17 @@ chmod o-wx ~/.ssh/
 chmod g-w ~/.ssh/config
 chmod o-wx ~/.ssh/config
 
-sudo apt-get update -y
-sudo apt-get install -y pigz
-cd $HOME
-pwd
-tar -cf jars.tar.gz -I pigz .gradle/caches/modules-2/files-2.1
-ls -al
+# sudo apt-get update -y
+# sudo apt-get install -y pigz
+# cd $HOME
+# pwd
+# tar -cf jars.tar.gz -I pigz .gradle/caches/modules-2/files-2.1
+# ls -al
 
+cd $HOME 
+sudo apt-get install p7zip-full -y
+7z a -r jars.7z .gradle/caches/modules-2/files-2.1
+ls -al
 
 
 dirs=(/home/travis/build/fy-travis-projects/genie/)
@@ -21,7 +25,7 @@ name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $dirs
 echo $name
 
-rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" jars.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" wrapper.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
