cd16370dd16e6652fd3a5f5ff978c0bf3347dae9
zfy <rachmanino@yahoo.com>
2020-04-29 22:48:11 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -10,18 +10,22 @@ chmod o-wx ~/.ssh/config
 sudo apt-get update -y
 sudo apt-get install -y pigz
 cd $HOME
-pwd
-tar -cf caches.tar.gz -I pigz .gradle/caches
-tar -cf wrapper.tar.gz -I pigz .gradle/wrapper
+# pwd
+# tar -cf caches.tar.gz -I pigz .gradle/caches
+# tar -cf wrapper.tar.gz -I pigz .gradle/wrapper
 # ls -al
+cd $HOME/.gradle/caches
+ls -al
+cd $HOME/.gradle/wrapper
+ls -al
 
 
-dirs=(/home/travis/build/fy-travis-projects/genie/)
-name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
-echo $dirs
-echo $name
+# dirs=(/home/travis/build/fy-travis-projects/genie/)
+# name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+# echo $dirs
+# echo $name
 
-rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" caches.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
-rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" wrapper.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+# rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" caches.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+# rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" wrapper.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
