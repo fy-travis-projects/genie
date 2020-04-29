194f26a2066a80791be02c33a9ce06e385f4f3a8
zfy <rachmanino@yahoo.com>
2020-04-29 18:30:36 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -13,7 +13,7 @@ chmod o-wx ~/.ssh/config
 # tar -cf repo.tar.gz -I pigz .gradle
 
 dirs=(~/build/penelope24/*/)
-name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+name="$(cut -d'/' -f5 <<<"${dirs[0]}")"
 echo $name
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" ~/repo.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
