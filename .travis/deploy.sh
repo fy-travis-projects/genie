96e6b8d9b4911edd9f835197771ee47d64f1193c
zfy <rachmanino@yahoo.com>
2020-04-30 23:57:00 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,11 +7,6 @@ chmod o-wx ~/.ssh/
 chmod g-w ~/.ssh/config
 chmod o-wx ~/.ssh/config
 
-# sudo apt-get update -y
-# sudo apt-get install -y pigz
-# cd $HOME
-# pwd
-# tar -cf jars.tar.gz -I pigz .gradle/caches/modules-2/files-2.1
 # ls -al
 
 # cd $HOME 
@@ -28,13 +23,21 @@ cd tmp
 pwd
 ls -al
 
+sudo apt-get update -y
+sudo apt-get install -y pigz
+cd $HOME
+pwd
+ls -al
+tar -cf project.tar.gz -I pigz tmp
+
+
 dirs=(/home/travis/build/fy-travis-projects/genie/)
 name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $name
 
 # scp -P 40501 -o stricthostkeychecking=no jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-# rsync -av -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --info=progress2 --inplace jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/jars.7z
+rsync -av -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --info=progress2 project.tar.gz qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 
