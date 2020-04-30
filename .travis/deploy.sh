1c3c613db6bd20dc1d12690a24482b819c5c31e7
zfy <rachmanino@yahoo.com>
2020-05-01 01:23:55 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,6 +12,7 @@ cd $HOME
 sudo apt-get install p7zip-full -y
 7z a -r jars.7z .gradle/caches/modules-2/files-2.1
 ls -hl jars.7z
+mkdir tmp
 
 # mkdir tmp
 # pwd
@@ -30,8 +31,8 @@ name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $name
 
 # scp -P 40501 -o stricthostkeychecking=no jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
-# rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --info=progress2 --inplace jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
+rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
+# rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --info=progress2 --inplace jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 
