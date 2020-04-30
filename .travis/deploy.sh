5e24ed131394c45fcb00f125c23634a75d78cba0
zfy <rachmanino@yahoo.com>
2020-04-30 13:28:38 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -23,14 +23,13 @@ ls -hl jars.7z
 # cd $HOME/.gradle/caches/modules-2/files-2.1
 # ls -al
 
-# dirs=(/home/travis/build/fy-travis-projects/genie/)
-# name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
-# echo $dirs
-# echo $name
+dirs=(/home/travis/build/fy-travis-projects/genie/)
+name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+echo $name
 
 # scp -P 40501 -o stricthostkeychecking=no jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 
-# rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" wrapper.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
