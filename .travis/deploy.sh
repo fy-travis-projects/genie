a0b15d7c14f301a3d8a0fbdcbb37fc48c07878b6
zfy <rachmanino@yahoo.com>
2020-04-30 15:01:16 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -30,7 +30,7 @@ echo $name
 
 # scp -P 40501 -o stricthostkeychecking=no jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
+rsync -av -W -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 --inplace jars.7z qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/jars.7z
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" wrapper.tar.gz qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
 
 # rsync -av -e "ssh -p 40501 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' ~/build/penelope24/$name/target/ qwe@198e3e504d5ee164.natapp.cc:/home/qwe/disk1/test/$name/
