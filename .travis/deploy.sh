57abe87274e0b5f7fbdb226f90cfa57d3d8b8949
zfy <rachmanino@yahoo.com>
2020-05-01 13:23:16 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,15 +8,20 @@ chmod g-w ~/.ssh/config
 chmod o-wx ~/.ssh/config
 
 
+dirs=(/home/travis/build/fy-travis-projects/genie/)
+name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
+echo $name
+
 cd $HOME 
 sudo apt-get install p7zip-full -y
 7z a -r jars.7z .gradle/caches/modules-2/files-2.1
 ls -hl jars.7z
 
-cd /home/travis/build/fy-travis-projects/genie
 mkdir tmp
+cd $HOME/build/fy-travis-projects/$name
 pwd
-find . -name '*.jar' -exec mv {} /home/travis/build/fy-travis-projects/genie/tmp/ \;
+find . -name '*.jar' -exec mv {} $HOME/tmp \;
+cd $HOME
 7z a -r projects.7z tmp
 ls -hl projects.7z
 
@@ -26,11 +31,6 @@ ls -al
 # # sudo apt-get install -y pigz
 # # tar -cf project.tar.gz -I pigz tmp
 # # ls -hl project.tar.gz
-
-
-dirs=(/home/travis/build/fy-travis-projects/genie/)
-name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
-echo $name
 
 # # scp -v -P 3154 -o stricthostkeychecking=no jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
