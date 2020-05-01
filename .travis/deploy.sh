b8820dd197f195d0341956077e69b1acaf83ec1e
zfy <rachmanino@yahoo.com>
2020-05-01 13:12:41 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,6 +14,7 @@ sudo apt-get install p7zip-full -y
 ls -hl jars.7z
 
 mkdir tmp
+pwd
 find . -name '*.jar' -exec mv {} /home/travis/build/fy-travis-projects/genie/tmp/ \;
 7z a -r projects.7z tmp
 ls -hl projects.7z
@@ -32,8 +33,8 @@ echo $name
 
 # # scp -v -P 3154 -o stricthostkeychecking=no jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 projects.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
+rsync -W -z -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
+# rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 projects.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
 # # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 
