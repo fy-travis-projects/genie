5fc9ae28b055036072c23149218eb4df1a630bc0
zfy <rachmanino@yahoo.com>
2020-05-01 13:34:58 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,15 +17,14 @@ sudo apt-get install p7zip-full -y
 7z a -r jars.7z .gradle/caches/modules-2/files-2.1
 ls -hl jars.7z
 
-mkdir tmp
-cd $HOME/build/fy-travis-projects/$name
-pwd
-find . -name '*.jar' -exec mv {} $HOME/tmp \;
-cd $HOME
-7z a -r projects.7z tmp
-ls -hl projects.7z
-
-ls -al
+# mkdir tmp
+# cd $HOME/build/fy-travis-projects/$name
+# pwd
+# find . -name '*.jar' -exec mv {} $HOME/tmp \;
+# cd $HOME
+# 7z a -r projects.7z tmp
+# ls -hl projects.7z
+# ls -al
 
 # # sudo apt-get update -y
 # # sudo apt-get install -y pigz
@@ -34,7 +33,7 @@ ls -al
 
 # # scp -v -P 3154 -o stricthostkeychecking=no jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -W -z -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
+rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
 # rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 projects.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
 # # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
