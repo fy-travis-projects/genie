3ac14bf13ee3cf4e160419b964bba11929f0ae2e
zfy <rachmanino@yahoo.com>
2020-05-01 14:01:40 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -35,7 +35,7 @@ cd $HOME
 
 # # scp -v -P 3154 -o stricthostkeychecking=no jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 .gradle/caches/modules-2/files-2.1 qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
+rsync -W -av -e "ssh -p 3154 -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 .gradle/caches/modules-2/files-2.1 qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
 # rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -x -T -o Compression=no" --info=progress2 projects.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
 # # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
