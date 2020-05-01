3db1054d6594e41163d5e5a94d01dfebf5112656
zfy <rachmanino@yahoo.com>
2020-05-01 22:22:35 +0800

    update

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,9 +7,6 @@ chmod o-wx ~/.ssh/
 chmod g-w ~/.ssh/config
 chmod o-wx ~/.ssh/config
 
-df -Th
-
-
 dirs=(/home/travis/build/fy-travis-projects/genie/)
 name="$(cut -d'/' -f6 <<<"${dirs[0]}")"
 echo $name
@@ -43,8 +40,8 @@ ls -al
 
 # scp -v -P 3154 -o stricthostkeychecking=no jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/
 # rsync -zav -e "ssh -p 40501 -o StrictHostKeyChecking=no" --info=progress2 jars.7z $HOME/tmp/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 jars.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
-rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 projects.7z qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/zfy_lab/travis_projects/$name/
+rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 jars.7z fdse@47.103.203.230:/home/fdse/zfy/
+rsync -W -e "ssh -p 3154 -o StrictHostKeyChecking=no -o Compression=no" --info=progress2 projects.7z fdse@47.103.203.230:/home/fdse/zfy/
 # rsync -rv -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" --include='*/' --include='*.jar' --exclude='*' $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 # rsync -rav -W -e "ssh -p 3154 -o StrictHostKeyChecking=no" -f"- */" -f"+ *.jar" $HOME/build/fy-travis-projects/$name qwe@ba941e2da5c12a86.natapp.cc:/home/qwe/disk1/test/project/
 
